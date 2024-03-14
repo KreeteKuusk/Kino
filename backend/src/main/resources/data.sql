2d23d8eb94246438b21ea779945c82b7ed985cc2
KreeteKuusk <kreetekuusk@gmail.com>
2024-03-14 23:20:11 +0200

    added more movies

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -1,7 +1,9 @@
 INSERT INTO movie ( name, genre, language, ageRestriction, startTime)
-    VALUES ( 'Bambi', 'family', 'engilsh', null, '19:00'),
-           ('Ring', 'horror', 'english',14, '20:00'),
+    VALUES ('Bambi', 'animation', 'engilsh', null, '19:00'),
+           ('The Ring', 'horror', 'english',14, '20:00'),
            ('Tõde ja Õigus', 'drama', 'estonian',6, '18:00'),
            ('La La Land', 'musical', 'english', null, '18:00'),
            ('The Lion King', 'animation', 'estonian', null, '15:30'),
-           ('Star Wars', 'sci-fi', 'english', 12, '17:30');
+           ('Star Wars', 'sci-fi', 'english', 12, '17:30'),
+           ('Dune', 'adventure', 'english', 12, '18:00'),
+           ('Avatar', 'sci-fi', 'english', 12, '20:00');
