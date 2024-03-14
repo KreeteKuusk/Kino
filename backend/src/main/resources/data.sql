c3c51e9b90b14f51af6253832ba7686c6fbdf7f5
KreeteKuusk <kreetekuusk@gmail.com>
2024-03-14 18:17:20 +0200

    small changes to data

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -1,2 +1,7 @@
 INSERT INTO movie ( name, genre, language, ageRestriction, startTime)
-    VALUES ( 'Bambi', 'familiy', 'engilsh', 1, '19:00');
+    VALUES ( 'Bambi', 'family', 'engilsh', null, '19:00'),
+           ('Ring', 'horror', 'english',14, '20:00'),
+           ('Tõde ja Õigus', 'drama', 'estonian',6, '18:00'),
+           ('La La Land', 'musical', 'english', null, '18:00'),
+           ('The Lion King', 'animation', 'estonian', null, '15:30'),
+           ('Star Wars', 'sci-fi', 'english', 12, '17:30');
