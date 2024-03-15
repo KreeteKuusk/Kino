883d602fa07b212977ad9a7a49a496dff98dedfa
KreeteKuusk <kreetekuusk@gmail.com>
2024-03-15 15:34:39 +0200

    Selecting seats works, but saving to database doesn't

--- backend/src/main/java/org/example/kino/controller/MovieController.java
+++ backend/src/main/java/org/example/kino/controller/MovieController.java
@@ -2,10 +2,7 @@ package org.example.kino.controller;
 
 import org.example.kino.model.Movie;
 import org.example.kino.repository.MovieRepository;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -18,16 +15,35 @@ public class MovieController {
         this.movieRepository = movieRepository;
     }
 
+    // for fetching all the movies
     @CrossOrigin
     @GetMapping("/api/movies")
     public List<Movie> getAllMovies() {
         return (List<Movie>) movieRepository.findAll();
     }
 
+    // for fetching a specific movie
     @CrossOrigin
     @GetMapping("/api/movies/{id}")
     public Movie getMovie(@PathVariable Integer id) throws Exception {
         return movieRepository.findById(id)
+                .orElseThrow(() -> new Exception("Movie not found with id " + id));
+    }
+
+    // for updating a specific movie (seats)
+    @CrossOrigin
+    @PutMapping("/api/movies/{id}")
+    public Movie updateMovie(@PathVariable Integer id, @RequestBody Movie updatedMovie) throws Exception {
+        return movieRepository.findById(id)
+                .map(movie -> {
+                    movie.setName(updatedMovie.getName());
+                    movie.setGenre(updatedMovie.getGenre());
+                    movie.setLanguage(updatedMovie.getLanguage());
+                    movie.setAgeRestriction(updatedMovie.getAgeRestriction());
+                    movie.setStartTime(updatedMovie.getStartTime());
+                    movie.setSeats(updatedMovie.getSeats());
+                    return movieRepository.save(movie);
+                })
                 .orElseThrow(() -> new Exception("Movie not found with id " + id));
     }
 
