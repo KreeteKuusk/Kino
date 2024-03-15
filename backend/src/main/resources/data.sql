28ea8c54223c1e274f95f7395c0df92b9908a09f
KreeteKuusk <kreetekuusk@gmail.com>
2024-03-15 12:40:19 +0200

    added more data and fixed typos

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -1,9 +1,12 @@
 INSERT INTO movie ( name, genre, language, ageRestriction, startTime)
-    VALUES ('Bambi', 'animation', 'engilsh', null, '19:00'),
+    VALUES ('Bambi', 'animation', 'english', null, '19:00'),
            ('The Ring', 'horror', 'english',14, '20:00'),
            ('Tõde ja Õigus', 'drama', 'estonian',6, '18:00'),
            ('La La Land', 'musical', 'english', null, '18:00'),
            ('The Lion King', 'animation', 'estonian', null, '15:30'),
            ('Star Wars', 'sci-fi', 'english', 12, '17:30'),
            ('Dune', 'adventure', 'english', 12, '18:00'),
-           ('Avatar', 'sci-fi', 'english', 12, '20:00');
+           ('Avatar', 'sci-fi', 'english', 12, '20:00'),
+           ('Wish', 'animation', 'estonian', null, '15:30'),
+           ('Shrek', 'animation', 'estonian', null, '16:30'),
+           ('Mamma Mia!', 'musical', 'english', null, '19:00');
